25bc73594bf4d407179c6a04cb2f8beaba56db6b
jakezappin <jake.zappin@gmail.com>
2016-11-11 09:36:43 -0500

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  var searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      var template = Handlebars.compile($('#results-template').html())
+      $('#results').html(template(data))
+    }).fail(error => {
+      displayError()
+    })
+}
+
+function showCommits(elem) {
+  var owner = elem.dataset.owner
+  var repo = elem.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    var template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
